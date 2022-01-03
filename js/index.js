cdcc55f234f7e3f2c8e69dca8d61d7a533417db6
Lucas Gaudio <lucas.gaudio@epimedsolutions.com>
2022-01-03 15:46:49 -0300

    Cria primeiros arquivos para o teste da datatable

---
+++ js/index.js
@@ -0,0 +1,655 @@
+var data = [
+  {
+    "Id": 500,
+    "Name": "Aminoglicosídeos",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 502,
+        "Name": "Gentamicina",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 501,
+        "Name": "Amicacina",
+        "Patients": 13,
+        "UseDays": 404,
+        "TotalDose": 342,
+        "DefinedDailyDose": 39.24,
+        "DaysOfTherapy": 46.35
+      }
+    ]
+  },
+  {
+    "Id": 1500,
+    "Name": "Anfotericina B",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 1504,
+        "Name": "Anfotericina B - Lipossomal",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 1501,
+        "Name": "Anfotericina B - Deoxicolato",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      }
+    ]
+  },
+  {
+    "Id": 1100,
+    "Name": "Carbapenêmicos",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 1102,
+        "Name": "Meropenem",
+        "Patients": 13,
+        "UseDays": 346,
+        "TotalDose": 533,
+        "DefinedDailyDose": 20.38,
+        "DaysOfTherapy": 39.7
+      },
+      {
+        "Id": 1101,
+        "Name": "Imipenem-cilastatina",
+        "Patients": 12,
+        "UseDays": 319,
+        "TotalDose": 667,
+        "DefinedDailyDose": 38.26,
+        "DaysOfTherapy": 36.6
+      },
+      {
+        "Id": 1103,
+        "Name": "Ertapenem",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 1104,
+        "Name": "Doripenem",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      }
+    ]
+  },
+  {
+    "Id": 200,
+    "Name": "Cefalosporinas",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 215,
+        "Name": "Cefuroxima axetil (oral)  Oral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 211,
+        "Name": "Cefuroxima (parenteral)  Parenteral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 221,
+        "Name": "Ceftriaxona",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 261,
+        "Name": "Ceftolozano / Tazobactam",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 241,
+        "Name": "Ceftazidima / Avibactam (Base ceftazidime)",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 222,
+        "Name": "Ceftazidima",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 212,
+        "Name": "Cefoxitina",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 224,
+        "Name": "Cefotaxima",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 240,
+        "Name": "Cefoperazona / Sulbactam (Base cefoperazona )",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 234,
+        "Name": "Cefoperazona",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 231,
+        "Name": "Cefepima",
+        "Patients": 12,
+        "UseDays": 311,
+        "TotalDose": 250.8,
+        "DefinedDailyDose": 7.19,
+        "DaysOfTherapy": 35.68
+      }
+    ]
+  },
+  {
+    "Id": 1600,
+    "Name": "Equiniocandinas",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 1603,
+        "Name": "Micafungina",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 1601,
+        "Name": "Caspofungina",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 1602,
+        "Name": "Anidulafungina",
+        "Patients": 11,
+        "UseDays": 224,
+        "TotalDose": 195.2,
+        "DefinedDailyDose": 223.96,
+        "DaysOfTherapy": 25.7
+      }
+    ]
+  },
+  {
+    "Id": 1000,
+    "Name": "Glicopeptídeos",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 1001,
+        "Name": "Vancomicina",
+        "Patients": 13,
+        "UseDays": 315,
+        "TotalDose": 362.8,
+        "DefinedDailyDose": 20.81,
+        "DaysOfTherapy": 36.14
+      },
+      {
+        "Id": 1002,
+        "Name": "Teicoplanina",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      }
+    ]
+  },
+  {
+    "Id": 1700,
+    "Name": "Imidazólicos",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 1703,
+        "Name": "Voriconazol  Parenteral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 1701,
+        "Name": "Fluconazol  Parenteral",
+        "Patients": 11,
+        "UseDays": 348,
+        "TotalDose": 44.5,
+        "DefinedDailyDose": 25.53,
+        "DaysOfTherapy": 39.93
+      }
+    ]
+  },
+  {
+    "Id": 900,
+    "Name": "Lincosamidas",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 901,
+        "Name": "Clindamicina  Parenteral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 901,
+        "Name": "Clindamicina  Oral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      }
+    ]
+  },
+  {
+    "Id": 1200,
+    "Name": "Lipopeptideos",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 1201,
+        "Name": "Daptomicina",
+        "Patients": 10,
+        "UseDays": 322,
+        "TotalDose": 279.7,
+        "DefinedDailyDose": 114.61,
+        "DaysOfTherapy": 36.94
+      }
+    ]
+  },
+  {
+    "Id": 700,
+    "Name": "Oxazolidinonas",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 701,
+        "Name": "Linezolida  Parenteral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 701,
+        "Name": "Linezolida  Oral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      }
+    ]
+  },
+  {
+    "Id": 100,
+    "Name": "Penicilinas",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 103,
+        "Name": "Piperacilina-tazobactam (Base piperacilina)",
+        "Patients": 1,
+        "UseDays": 11,
+        "TotalDose": 88.01,
+        "DefinedDailyDose": 0.72,
+        "DaysOfTherapy": 1.26
+      },
+      {
+        "Id": 105,
+        "Name": "Ampicilina-sulbactam (Base sulbactam)",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 104,
+        "Name": "Amoxicilina / Ac. Clavulânico (Base amoxicilina)  Parenteral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 104,
+        "Name": "Amoxicilina / Ac. Clavulânico (Base amoxicilina)  Oral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      }
+    ]
+  },
+  {
+    "Id": 1300,
+    "Name": "Polimixinas",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 1301,
+        "Name": "Polimixina B",
+        "Patients": 11,
+        "UseDays": 402,
+        "TotalDose": 217.7,
+        "DefinedDailyDose": 166.51,
+        "DaysOfTherapy": 46.12
+      },
+      {
+        "Id": 1302,
+        "Name": "Colistina (Polimixina E)",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      }
+    ]
+  },
+  {
+    "Id": 300,
+    "Name": "Quinolonas",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 305,
+        "Name": "Moxifloxacino  Parenteral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 305,
+        "Name": "Moxifloxacino  Oral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 302,
+        "Name": "Levofloxacina  Parenteral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 302,
+        "Name": "Levofloxacina  Oral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 317,
+        "Name": "Gatifloxacina  Parenteral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 317,
+        "Name": "Gatifloxacina  Oral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 301,
+        "Name": "Ciprofloxacina  Parenteral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      },
+      {
+        "Id": 301,
+        "Name": "Ciprofloxacina  Oral",
+        "Patients": null,
+        "UseDays": null,
+        "TotalDose": null,
+        "DefinedDailyDose": null,
+        "DaysOfTherapy": null
+      }
+    ]
+  },
+  {
+    "Id": 600,
+    "Name": "Tetraciclinas / Glicilciclinas",
+    "AntimicrobialAgentDoses": [
+      {
+        "Id": 603,
+        "Name": "Tigeciclina",
+        "Patients": 11,
+        "UseDays": 337,
+        "TotalDose": 271.9,
+        "DefinedDailyDose": 311.96,
+        "DaysOfTherapy": 38.66
+      }
+    ]
+  }
+];
+
+var newData = [
+  [
+    "Aminoglicosídeos",
+    "Gentamicina",
+    8,
+    101,
+    120.92,
+    33.53,
+    13.20
+  ],
+  [
+    "Aminoglicosídeos",
+    "Amicacina",
+    10,
+    404,
+    81.90,
+    23.53,
+    23.20
+  ],
+  [
+    "Anfotericina B",
+    "Anfotericina B - Lipossomal",
+    7,
+    301,
+    150.92,
+    3.53,
+    3.20
+  ],
+  [
+    "Anfotericina B",
+    "Anfotericina B - Deoxicolato",
+    19,
+    454,
+    89.90,
+    63.53,
+    53.20
+  ],
+  [
+    "Carbapenêmicos",
+    "Meropenem",
+    8,
+    501,
+    190.92,
+    3.43,
+    3.20
+  ],
+  [
+    "Carbapenêmicos",
+    "Imipenem-cilastatina",
+    7,
+    301,
+    160.92,
+    3.03,
+    3.20
+  ], [
+    "Carbapenêmicos",
+    "Ertapenem",
+    7,
+    301,
+    190.92,
+    36.13,
+    6.20
+  ], [
+    "Carbapenêmicos",
+    "Doripenem",
+    4,
+    301,
+    50.92,
+    8.53,
+    8.20
+  ],
+];
+
+function createExecutivePanelTable(antimicrobialsClass, tableId, initialIndex, finalIndex) {
+  for (var i = initialIndex; i < finalIndex + 1; i++) {
+    var antimicrobialClassElement =
+      '<tr class="antimicrobial-doses__table-body-row" id="antimicrobial-doses__table-body-row-' + antimicrobialsClass[i].Id + '">' +
+      '<th colspan=6 class="antimicrobial-doses__table-body-header">' +
+      antimicrobialsClass[i].Name +
+      '</th>' +
+      '</tr>';
+
+    $(tableId).append(antimicrobialClassElement);
+
+    antimicrobialsClass[i].AntimicrobialAgentDoses.forEach(function (antimicrobial) {
+      var antimicrobialPatients = antimicrobial.Patients == null ? '-' : antimicrobial.Patients;
+
+      var antimicrobialUseDays = antimicrobial.UseDays == null ? '-' : antimicrobial.UseDays;
+
+      var antimicrobialTotalDose = antimicrobial.TotalDose == null ? '-' : antimicrobial.TotalDose;
+
+      var antimicrobialDefinedDailyDose = antimicrobial.DefinedDailyDose == null ? '-' : antimicrobial.DefinedDailyDose;
+
+      var antimicrobialDaysOfTherapy = antimicrobial.DaysOfTherapy == null ? '-' : antimicrobial.DaysOfTherapy;
+
+
+      var antimicrobialsElement =
+        '<tr class="antimicrobial-report-table antimicrobial-doses__table-body-row antimicrobial-doses__table-body-line-row">' +
+        '<td class="antimicrobial-doses__table-body-line antimicrobial-doses__table-body-line-name" >' +
+        antimicrobial.Name +
+        '</td>' +
+        '<td class="antimicrobial-doses__table-body-line antimicrobial-doses__table-body-line-patients" >' +
+        antimicrobialPatients +
+        '</td>' +
+        '<td class="antimicrobial-doses__table-body-line antimicrobial-doses__table-body-line-days-of-use" >' +
+        antimicrobialUseDays +
+        '</td>' +
+        '<td class="antimicrobial-doses__table-body-line" >' +
+        antimicrobialTotalDose +
+        '</td>' +
+        '<td class="antimicrobial-doses__table-body-line" >' +
+        antimicrobialDefinedDailyDose +
+        '</td>' +
+        '<td class="antimicrobial-doses__table-body-line" >' +
+        antimicrobialDaysOfTherapy +
+        '</td>' +
+        '</tr>';
+
+      return $("#antimicrobial-doses__table-body-row-" + antimicrobialsClass[i].Id).after(antimicrobialsElement);
+    });
+  };
+}
+
+$(document).ready(function () {
+  createExecutivePanelTable(data, "#antimicrobial-doses__table-body--one", 0, 5);
+
+  $('#antimicrobial-doses__table--one').DataTable({
+    scrollY: "300px",
+    scrollX: true,
+    scrollCollapse: true,
+    paging: false,
+    ordering: false,
+    data: newData,
+    fixedColumns: {
+      left: 1,
+    },
+    paging: false,
+    searching: false,
+    columnDefs: [
+      { "visible": false, "targets": 0 }
+    ],
+    order: [[0, 'asc']],
+    rowGroup: {
+      dataSrc: 0,
+      visible: false
+
+    },
+
+
+  });
+});
+
+
+
